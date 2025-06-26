c8f2ae36574fe964b670ad9764b885a078507934
Oleg Budaev <t0r1n88@mail.ru>
2025-06-26 08:43:11 +0800

    исправил порядок при сортировке, могла быть ситуация когда выпадали

--- adult_tests/processing_adults_complex.py
+++ adult_tests/processing_adults_complex.py
@@ -384,7 +384,7 @@ if __name__ == '__main__':
 
 
     main_end_folder = 'c:/Users/1/PycharmProjects/Lachesis/data/Результат'
-    main_quantity_descr_cols = 3
+    main_quantity_descr_cols = 4
     main_svod_cols = '1,2,3'
 
     generate_result_adults(main_params_adults, main_adults_data, main_end_folder, main_quantity_descr_cols,main_svod_cols)
