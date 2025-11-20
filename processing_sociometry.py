ec070d8e764f7e098ccbb2282e31a978f0b13d03
Oleg Budaev <t0r1n88@mail.ru>
2025-11-20 12:36:36 +0800

    релизовал групповые индексы

--- processing_sociometry.py
+++ processing_sociometry.py
@@ -3,10 +3,27 @@
 """
 import pandas as pd
 import openpyxl
-from collections import Counter
 import time
 import copy
-from collections import defaultdict
+import re
+
+
+
+class NotReqColumn(Exception):
+    """
+    Исключение для обработки случая когда нет обязательной колонки ФИО
+    """
+    pass
+
+class BadQuantNegCols(Exception):
+    """
+    Исключение для обработки случая когда указан номер колонки с негативным вопросом большем чем есть вопросов в файле
+    """
+    pass
+
+
+
+
 
 def extract_answer_several_option(row:pd.Series):
     """
@@ -59,17 +76,42 @@ def calc_quantity_change(value):
         return 0
 
 
+def check_negative_cols(quant_questions:int, negative_str:str):
+    """
+    Функция для проверки наличия колонок с негативными вопросами
+    :param quant_questions: количество вопросов
+    :param negative_str: строка с перечислением колонок или пустая
+    :return: список или пустой список
+    """
+    result = re.findall(r'\d+', negative_str)
+    out_lst = [] # список для хранения номеров колонок с негативными вопросами
+    if result:
+        for value in result:
+            value_digit = int(value)
+            if value_digit > quant_questions:
+                raise BadQuantNegCols
+            if value_digit == 0: # на случай если записан ноль
+                out_lst.append(1)
+            else:
+                out_lst.append(value_digit-1)
+        return out_lst
+    else:
+        return []
 
 
 
 
-def generate_result_sociometry(data_file:str,quantity_descr_cols:int,end_folder:str):
+
+
+def generate_result_sociometry(data_file:str,quantity_descr_cols:int,negative_questions:str,end_folder:str):
     """
     Функция для генерации результатов социометрического исследования
     :param data_file: файл с данными из форм
     :param quantity_descr_cols: количество анкетных колонок
+    :param negative_questions: строка с порядковыми номерам негативных вопросов
     :param end_folder: конечная папка
     """
+
     t = time.localtime()
     current_time = time.strftime('%H_%M_%S', t)
     checked_dct = dict()  # словарь для хранения проверочных датафреймов по каждому вопросу
@@ -80,6 +122,11 @@ def generate_result_sociometry(data_file:str,quantity_descr_cols:int,end_folder:
 
 
     base_df = pd.read_excel(data_file,dtype=str) # исходный датафрейм
+    # Проверяем наличие колонки ФИО
+    diff_req_cols = {'ФИО'}.difference(set(base_df.columns))
+    if len(diff_req_cols) != 0:
+        raise NotReqColumn
+
     base_df = base_df[base_df['ФИО'].notna()] # удаляем незаполенные строки в колонке ФИО
     # очищаем от лишних пробелов в начале и конце
     base_df = base_df.applymap(lambda x:x.strip() if isinstance(x,str) else x)
@@ -89,8 +136,6 @@ def generate_result_sociometry(data_file:str,quantity_descr_cols:int,end_folder:
     template_matrix_df = pd.DataFrame(index=base_df['ФИО'].tolist(),columns=base_df['ФИО'].tolist())
 
     lst_fio = base_df['ФИО'].tolist() # делаем список ФИО
-
-
 
 
     # создаем словарь где вида {ФИО:{ФИО:0}}
@@ -110,8 +155,20 @@ def generate_result_sociometry(data_file:str,quantity_descr_cols:int,end_folder:
         if question not in lst_questions:
             lst_questions.append(question)
 
+    # проверяем и обрабатываем строку с колонками по которым нужно делать свод
+    lst_negative_cols = check_negative_cols(len(lst_questions), negative_questions)
+
     ind_templ_dct = {f'Вопрос_{number}':0 for number in range(1,len(lst_questions)+1)}
-    index_dct = {'Индекс групповой сплоченности (Сn)':copy.deepcopy(ind_templ_dct)} # словарь для хранения групповых индексов
+    # В зависимости от того есть ли колонки с негативными ответами
+    if len(lst_negative_cols) == 0:
+        index_dct = {'Индекс эмоциональной экспансивности группы':copy.deepcopy(ind_templ_dct),
+            'Индекс групповой сплоченности':copy.deepcopy(ind_templ_dct),
+                     'Индекс референтности группы':copy.deepcopy(ind_templ_dct)} # словарь для хранения групповых индексов
+    elif len(lst_negative_cols) == len(lst_questions):
+        index_dct = {'Индекс эмоциональной экспансивности группы':copy.deepcopy(ind_templ_dct),
+            'Индекс групповой конфликтности': copy.deepcopy(ind_templ_dct),
+                     'Индекс референтности группы': copy.deepcopy(ind_templ_dct)
+                     }  # словарь для хранения групповых индексов
 
 
     for idx, name_question in enumerate(lst_questions,1):
@@ -147,15 +204,32 @@ def generate_result_sociometry(data_file:str,quantity_descr_cols:int,end_folder:
         change_row = [len(value) for key,value in change_dct.items()]
         # считаем количество выборов
         sum_row = one_matrix_df.sum()
-        one_matrix_df.loc['Кол-во выборов'] = sum_row
-        one_matrix_df.loc['Кол-взаимных выборов'] = change_row
+        if len(lst_negative_cols) == 0:
+            one_matrix_df.loc['Кол-во выборов'] = sum_row
+            one_matrix_df.loc['Кол-взаимных выборов'] = change_row
+        elif len(lst_negative_cols) == len(lst_questions):
+            one_matrix_df.loc['Кол-во негативных выборов'] = sum_row
+            one_matrix_df.loc['Кол-во негативных взаимных выборов'] = change_row
+        else:
+            print('mix')
+
 
         matrix_dct[idx] = one_matrix_df # добавляем в словарь для сохранения
 
-        # Считаем индексы
-        max_sum_mutual_change = (len(base_df)* (len(base_df)-1)) / 2 # максимально возможное число взаимных выборов
-        cn_index = sum(change_row) / max_sum_mutual_change # Индекс групповой сплоченности
-        index_dct['Индекс групповой сплоченности (Сn)'][f'Вопрос_{idx}'] = cn_index
+        if len(lst_negative_cols) == 0:
+            # Считаем индексы
+            max_sum_mutual_change = (len(base_df)* (len(base_df)-1)) / 2 # максимально возможное число взаимных выборов
+            cn_index = sum(change_row) / max_sum_mutual_change # Индекс групповой сплоченности
+            index_dct['Индекс групповой сплоченности'][f'Вопрос_{idx}'] = round(cn_index,2)
+            index_dct['Индекс эмоциональной экспансивности группы'][f'Вопрос_{idx}'] = round(sum(sum_row) / len(base_df),2)
+            index_dct['Индекс референтности группы'][f'Вопрос_{idx}'] = round(sum(change_row) / sum(sum_row),2)
+        elif len(lst_negative_cols) == len(lst_questions):
+            # Считаем индексы
+            max_sum_mutual_change = (len(base_df)* (len(base_df)-1)) / 2 # максимально возможное число взаимных выборов
+            conf_index = sum(change_row) / max_sum_mutual_change # Индекс групповой конфликтности
+            index_dct['Индекс групповой конфликтности'][f'Вопрос_{idx}'] = round(conf_index,2)
+            index_dct['Индекс эмоциональной экспансивности группы'][f'Вопрос_{idx}'] = round(sum(sum_row) / len(base_df),2)
+            index_dct['Индекс референтности группы'][f'Вопрос_{idx}'] = round(sum(change_row) / sum(sum_row),2)
 
 
     # Суммируем словари с ответами на отдельные вопросы для получения общего словаря
@@ -183,9 +257,10 @@ def generate_result_sociometry(data_file:str,quantity_descr_cols:int,end_folder:
 
     union_df.index = lst_index_union
     # Создаем колонки с добавлением цифр
-    # union_df.columns = range(1,len(lst_fio)+1)
+    union_df.columns = range(1,len(lst_fio)+1)
     # Подсчитываем колонку Итого
-    union_df['Итого'] = union_df.apply(calc_itog,axis=1)
+    union_df['Итого выборов'] = union_df.apply(calc_itog,axis=1)
+
 
     union_df.to_excel(f'{end_folder}/Общая социоматрица {current_time}.xlsx',index=True)
 
@@ -212,7 +287,7 @@ def generate_result_sociometry(data_file:str,quantity_descr_cols:int,end_folder:
             # Создаем колонки с добавлением цифр
             # out_df.columns = range(1,len(lst_fio)+1)
             # Подсчитываем колонку Итого
-            out_df['Итого'] = out_df.apply(calc_itog, axis=1)
+            out_df['Итого выборов'] = out_df.apply(calc_itog, axis=1)
 
             out_df.to_excel(writer,sheet_name=str(name_sheet),index=True)
 
@@ -237,7 +312,9 @@ def generate_result_sociometry(data_file:str,quantity_descr_cols:int,end_folder:
 
 if __name__ == '__main__':
     main_file = 'data/Социометрия.xlsx'
-    main_quantity_descr_cols = 2
+    main_file = 'data/Социометрия негатив.xlsx'
+    main_quantity_descr_cols = 1
+    main_negative_questions = '1,2,3'
     main_end_folder = 'data/Результат'
-    generate_result_sociometry(main_file,main_quantity_descr_cols,main_end_folder)
+    generate_result_sociometry(main_file,main_quantity_descr_cols,main_negative_questions,main_end_folder)
     print('Lindy Booth')
